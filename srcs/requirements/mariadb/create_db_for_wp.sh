d015ca81941953b5bda80f815e5d2f1770819b83
Nhung-Ubuntu-Home <nnhung2@gmail.com>
2022-03-03 10:25:39 +0100

    ok

--- srcs/requirements/mariadb/create_db_for_wp.sh
+++ srcs/requirements/mariadb/create_db_for_wp.sh
@@ -1,36 +1,95 @@
-# if [ -d "/run/bin/mysqld" ]; then
-# 	echo "[i] mysqld already present, skipping creation"
-# 	chown -R www-data:www-data /run/bin/
-# 	mysqld
-# else
-# 	echo "[i] mysqld not found, creating...."
-# 	mkdir -p /run/bin/mysqld
-# 	chown -R www-data:www-data /run/bin/mysqld
-# fi
-
-service mysql start
-
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;"
-
-mysql -u root -e "GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;"
-mysql -u root -e "GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;"
-mysql -u root -e "SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}'); DROP DATABASE IF EXISTS test; FLUSH PRIVILEGES; "
-
-mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
-
-mysql -u root -e "USE $MYSQL_DATABASE; GRANT ALL PRIVILEGES ON * TO '$MYSQL_USER'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-
-mysql -u root -e "INSERT INTO $MYSQL_DATABASE.wp_users (user_login,user_pass,user_nicename,user_email,user_url,user_registered,user_activation_key,user_status,display_name) VALUES ('thi-nguy',MD5('thi-nguy'),'','thi-nguy@example.com','https://thi-nguy.42.fr','','',0,'');"
+# # if [ -d "/run/bin/mysqld" ]; then
+# # 	echo "[i] mysqld already present, skipping creation"
+# # 	chown -R www-data:www-data /run/bin/
+# # 	mysqld
+# # else
+# # 	echo "[i] mysqld not found, creating...."
+# # 	mkdir -p /run/bin/mysqld
+# # 	chown -R www-data:www-data /run/bin/mysqld
+# # fi
 
 
-# ! Problem here
-# https://stackoverflow.com/questions/11657829/error-2002-hy000-cant-connect-to-local-mysql-server-through-socket-var-run
+# # mysql -u root -h"$WORDPRESS_DB_HOST" -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD
 
-# This does not work
-echo "bind-address=0.0.0.0" >> /etc/mysql/my.cnf
+# mysql -u root -h0.0.0.0 -P3306 
+# # service mysql start
 
-# mysql -h 127.0.0.1 -P 3306 -u root -p $MYSQL_DATABASE
-exec mysqld && mariadb
+# mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;"
+
+# mysql -u root -e "GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;"
+
+# mysql -u root -e "GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;"
+# mysql -u root -e "SET PASSWORD FOR 'root'@'localhost'='$MYSQL_ROOT_PASSWORD'); DROP DATABASE IF EXISTS test; FLUSH PRIVILEGES; "
+
+# mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
+
+# mysql -u root -e "USE $MYSQL_DATABASE; GRANT ALL PRIVILEGES ON * TO '$MYSQL_USER'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+
+# # mysql -u root -e "INSERT INTO $MYSQL_DATABASE.wp_users (user_login,user_pass,user_nicename,user_email,user_url,user_registered,user_activation_key,user_status,display_name) VALUES ('thi-nguy',MD5('thi-nguy'),'','thi-nguy@example.com','https://thi-nguy.42.fr','','',0,'');"
 
 
+# # ! Problem here
+# # https://stackoverflow.com/questions/11657829/error-2002-hy000-cant-connect-to-local-mysql-server-through-socket-var-run
 
+# # This does not work
+# # echo "bind-address=0.0.0.0" >> /etc/mysql/my.cnf
+# sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/my.cnf
+
+# exec mysqld && mariadb
+
+
+# # sr/bin/mysqld --user=mysql --console
+
+
+#!/bin/sh
+
+if [ -d "/run/mysqld" ]; then
+	echo "[i] mysqld already present, skipping creation"
+	chown -R mysql:mysql /run/mysqld
+else
+	echo "[i] mysqld not found, creating...."
+	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
+fi
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	
+	chown -R mysql:mysql /var/lib/mysql
+
+	# init database
+	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+
+	tfile=`mktemp`
+	if [ ! -f "$tfile" ]; then
+		return 1
+	fi
+
+	# https://stackoverflow.com/questions/10299148/mysql-error-1045-28000-access-denied-for-user-billlocalhost-using-passw
+	cat << EOF > $tfile
+USE mysql;
+FLUSH PRIVILEGES ;
+GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
+SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+DROP DATABASE IF EXISTS test ;
+FLUSH PRIVILEGES ;
+
+CREATE DATABASE $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_USER_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED by '$MYSQL_USER_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';
+
+FLUSH PRIVILEGES;
+EOF
+	# run init.sql
+	/usr/bin/mysqld --user=mysql --bootstrap < $tfile
+	rm -f $tfile
+fi
+
+# allow remote connections
+# https://wiki.alpinelinux.org/wiki/MariaDB
+sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+
+exec /usr/bin/mysqld --user=mysql --console
--- srcs/requirements/wordpress/install_wp.sh
+++ srcs/requirements/wordpress/install_wp.sh
@@ -7,9 +7,9 @@
 
 if [ ! -f "/var/www/html/wordpress/index.php" ]; then
     wp core download --allow-root
-    wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbcharset="utf8" --allow-root 
-    wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root 
 fi 
+wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbcharset="utf8" --allow-root
+wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root 
 # wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root 
 # wp theme install blocksy --activate --allow-root 
 # wp plugin update --all --allow-root
